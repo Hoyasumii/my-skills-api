0ff223c0367f1e239f59fa4b62449cd5619258bb
Hoyasumii <alanreisanjo@gmail.com>
2025-03-10 12:15:24 -0300

    Ajustes

--- src/routes/picture-routes.ts
+++ src/routes/picture-routes.ts
@@ -5,7 +5,7 @@ import { Hono } from "hono";
 
 export const pictureRoutes = new Hono();
 
-pictureRoutes.get("/", zValidator("query", GetPictureModel), async (c) => {
+pictureRoutes.get("/get", zValidator("query", GetPictureModel), async (c) => {
   const data = c.req.valid("query");
 
   const service = makeGettingPicture();
